c4c88b7d235f56c7e0d93e97b77ffa41eb0281e4
Kanishka <kanishkamakhija007@gmail.com>
2017-07-21 19:24:19 +0530

    pep8 updated and comments added

--- media.py
+++ media.py
@@ -1,14 +1,22 @@
 import webbrowser
+
+
 class Movie():
     """
         class defines the basic structure of Movie objects.
     """
 
-    def __init__(self,title,description, poster, link_trailer):
+    def __init__(self, title, description, poster, link_trailer):
+        """
+            This is the constructor function which
+            takes the following arguments
+            1. Title
+            2. Description
+            3. Poster image link
+            4. Movie trailer link
+            This function then initialises the instance variable
+        """
         self.title = title
         self.description = description
-        self.poster= poster
+        self.poster = poster
         self.link_trailer = link_trailer
-
-    def open_trailer(self):
-        webbrowser.open(self.link_trailer)
--- movie.py
+++ movie.py
@@ -1,24 +1,69 @@
 import media
 import fresh_tomatoes
 
-#this contains list of movies
+# This file contains list of movie objects
 
-love_story = media.Movie("Love Rosie", "love story", "http://www.impawards.com/2014/posters/love_rosie_ver5_xlg.jpg", "https://www.youtube.com/watch?v=Te8rfiN6_Qo")
-#print(love_story.storyline)
-avatar = media.Movie("Avatar", "marine on alien planet", "http://moviecultists.com/wp-content/uploads/2009/11/avatar-poster.jpg", "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+love_story = media.Movie("Love Rosie",
+                         "love story",
+                         "http://www.impawards.com/2014/posters/love_rosi"
+                         "e_ver5_xlg.jpg",
+                         "https://www.youtube.com/watch?v=5zL3YJKygd4")
 
-now_you_see_me = media.Movie("Now You See Me", "The Four Horsemen resurface and are forcibly recruited by a tech genius to pull off their most impossible heist yet.", "http://www.impawards.com/2016/posters/now_you_see_me_two_ver23_xxlg.jpg", "https://www.youtube.com/watch?v=4I8rVcSQbic")
+avatar = media.Movie("Avatar",
+                     "marine on alien planet",
+                     "http://moviecultists.com/wp-content/upl"
+                     "oads/2009/11/avatar-poster.jpg",
+                     "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
-a_beautiful_mind = media.Movie("A Beautiful Mind", "A Beautiful Mind is a 2001 American biographical drama film based on the life of John Nash, a Nobel Laureate in Economics.","https://media.licdn.com/mpr/mpr/AAEAAQAAAAAAAAk5AAAAJDA3ZmFmY2RjLWRlNTktNGUyMS1hMmE3LTU5NTZhOTAxOWRkNQ.jpg", "https://fmovies.is/film/a-beautiful-mind.rlxm")
+now_you_see_me = media.Movie("Now You See Me",
+                             "The Four Horsemen resurface and are forcibly"
+                             "recruited by a"
+                             "tech genius to pull off their"
+                             " most impossible heist yet.",
+                             "http://www.impawards.com/2016"
+                             "/posters/now_you_see_me_two_ver23_xxlg.jpg",
+                             "https://www.youtube.com/watch?v=KzJNYYkkhzc")
 
-divergent = media.Movie("Divergent", "Tris, an adult resident of a futuristic world divided into five factions, elects to join the Dauntless faction. But she actually belongs to another faction, which she must hide, as a major war looms.","http://resizing.flixster.com/ATk50ODLNE5Kua1DSOgST5ct-38=/800x1200/v1.bTsxMTE3ODkxMDtqOzE3NDE5OzIwNDg7ODAwOzEyMDA", "https://fmovies.is/film/divergent.2950")
+a_beautiful_mind = media.Movie("A Beautiful Mind",
+                               "A Beautiful Mind is a 2001 Am"
+                               "erican biographical drama"
+                               "film based on the life of"
+                               " John Nash, a Nobel Laureate"
+                               "in Economics.",
+                               "https://media.licdn.com/mpr/mp"
+                               "r/AAEAAQAAAAAAAAk5AAAAJDA"
+                               "3ZmFmY2RjLWRlNTktNGUyMS1hMmE3L"
+                               "TU5NTZhOTAxOWRkNQ.jpg",
+                               "https://www.youtube.com/watch?v=aS_d0Ayjw4o")
 
-insurgent = media.Movie("Insurgent" , "The Divergent Series: Insurgent (also known simply as Insurgent) is a 2015 American science fiction action film directed by Robert Schwentke, based on Insurgent, the second book in the Divergent trilogy by Veronica Roth. It is the sequel to the 2014 film Divergent and the second installment in The Divergent Series","https://images-na.ssl-images-amazon.com/images/I/51uewbwd7nL.jpg", "https://xmovies8.org/watch?v=Insurgent_2015")
-#print(avatar.storyline)
-#love_story.show_trailer()
-movies = [love_story, avatar, now_you_see_me,a_beautiful_mind,divergent,insurgent]
+divergent = media.Movie("Divergent",
+                        "Tris, an adult resident of a futuristic world"
+                        "divided into five factions, elects to join the"
+                        "Dauntless faction. But she actually belongs to"
+                        "another faction, which she must hide, as a major"
+                        "war looms.",
+                        "http://resizing.flixster.com/ATk50ODLNE5Kua1DSOg"
+                        "ST5ct-38=/800x1200/v1.bTsxMTE3ODk"
+                        "xMDtqOzE3NDE5OzIwNDg7ODAwOzEyMDA",
+                        "https://www.youtube.com/watch?v=sutgWjz10sM")
+
+insurgent = media.Movie("Insurgent",
+                        "The Divergent Series: Insurgent (also known simpl"
+                        "y as Insurgent) is a 2015 American science fiction"
+                        "action film directed by Robert Schwentke, based"
+                        "on Insurgent, the second book in the Divergent"
+                        "trilogy by Veronica Roth. It is the sequel to"
+                        "the 2014 film Divergent and the second installment"
+                        "in The Divergent Series",
+                        "https://images-na.ssl-images-amazon.com/imag"
+                        "es/I/51uewbwd7nL.jpg",
+                        "https://www.youtube.com/watch?v=UtgPdoxU-SY")
+
+movies = [love_story,
+          avatar,
+          now_you_see_me,
+          a_beautiful_mind,
+          divergent,
+          insurgent]
+
 fresh_tomatoes.open_movies_page(movies)
-#print(movie.Movie.VALID_RATINGS)
-#print(movie.Movie.__doc__)
-#print(movie.Movie.__name__)
-#print(movie.Movie.__module__)
